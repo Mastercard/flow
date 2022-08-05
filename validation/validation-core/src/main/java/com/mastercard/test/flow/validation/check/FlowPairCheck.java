25caf04227fb48ca42a99aee5cc90fe65925abed
Ryan McNally <ryan.mcnally@mastercard.com>
2022-08-05 16:11:12 +0100

    Duplication (#27)
    * removed duplication
    
    * more deduplication, and a better approach to sonar config
    
    * format

---
+++ validation/validation-core/src/main/java/com/mastercard/test/flow/validation/check/FlowPairCheck.java
@@ -0,0 +1,72 @@
+package com.mastercard.test.flow.validation.check;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import com.mastercard.test.flow.Flow;
+import com.mastercard.test.flow.Model;
+import com.mastercard.test.flow.validation.Check;
+import com.mastercard.test.flow.validation.Validation;
+import com.mastercard.test.flow.validation.Violation;
+
+/**
+ * Convenience superclass for validation checks that compare pairs of flows
+ */
+public abstract class FlowPairCheck implements Validation {
+
+	private final String name;
+	private final String explanation;
+
+	/**
+	 * @param name        to be returned from {@link #name()}
+	 * @param explanation to be returned from {@link #explanation()}
+	 */
+	protected FlowPairCheck( String name, String explanation ) {
+		this.name = name;
+		this.explanation = explanation;
+	}
+
+	@Override
+	public String name() {
+		return name;
+	}
+
+	@Override
+	public String explanation() {
+		return explanation;
+	}
+
+	@Override
+	public Stream<Check> checks( Model model ) {
+		List<Check> checks = new ArrayList<>();
+		Flow[] flows = model.flows().toArray( Flow[]::new );
+
+		for( int i = 0; i < flows.length; i++ ) {
+			Flow left = flows[i];
+			for( int j = i + 1; j < flows.length; j++ ) {
+				Flow right = flows[j];
+
+				checks.add( new Check( this,
+						left.meta().id() + " x " + right.meta().id(),
+						() -> violation( left, right )
+								.map( v -> new Violation( this, v, null, null )
+										.offender( left )
+										.offender( right ) )
+								.orElse( null ) ) );
+			}
+		}
+		return checks.stream();
+
+	}
+
+	/**
+	 * Implement this with your pair-checking logic
+	 *
+	 * @param left  A flow
+	 * @param right The other flow
+	 * @return violation text, if a problem is detected
+	 */
+	protected abstract Optional<String> violation( Flow left, Flow right );
+}
