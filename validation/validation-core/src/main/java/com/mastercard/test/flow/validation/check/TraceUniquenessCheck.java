cf1a19cf7eafd37cc7e671ed2233a320aee84f5d
Ryan McNally <ryan.mcnally@mastercard.com>
2022-08-05 15:38:44 +0100

    removed duplication

--- validation/validation-core/src/main/java/com/mastercard/test/flow/validation/check/TraceUniquenessCheck.java
+++ validation/validation-core/src/main/java/com/mastercard/test/flow/validation/check/TraceUniquenessCheck.java
@@ -1,53 +1,26 @@
 package com.mastercard.test.flow.validation.check;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Stream;
+import java.util.Optional;
 
 import com.mastercard.test.flow.Flow;
 import com.mastercard.test.flow.Model;
-import com.mastercard.test.flow.validation.Check;
-import com.mastercard.test.flow.validation.Validation;
-import com.mastercard.test.flow.validation.Violation;
 
 /**
  * Checks that all {@link Flow}s in a {@link Model} have a unique trace
  */
-public class TraceUniquenessCheck implements Validation {
+public class TraceUniquenessCheck extends FlowPairCheck {
 
-	@Override
-	public String name() {
-		return "Trace uniqueness";
+	/***/
+	public TraceUniquenessCheck() {
+		super( "Trace uniqueness", "All flows in a model have a unique trace" );
 	}
 
 	@Override
-	public String explanation() {
-		return "All flows in a model have a unique trace";
-	}
-
-	@Override
-	public Stream<Check> checks( Model model ) {
-		List<Check> checks = new ArrayList<>();
-		Flow[] flows = model.flows().toArray( Flow[]::new );
-
-		for( int i = 0; i < flows.length; i++ ) {
-			Flow a = flows[i];
-			for( int j = i + 1; j < flows.length; j++ ) {
-				Flow b = flows[j];
-
-				checks.add( new Check( this,
-						a.meta().id() + " x " + b.meta().id(),
-						() -> {
-							if( a.meta().trace().equals( b.meta().trace() ) ) {
-								return new Violation( this, "Shared trace", null, null )
-										.offender( a )
-										.offender( b );
-							}
-							return null;
-						} ) );
-			}
+	protected Optional<String> violation( Flow left, Flow right ) {
+		if( left.meta().trace().equals( right.meta().trace() ) ) {
+			return Optional.of( "Shared trace" );
 		}
-		return checks.stream();
+		return Optional.empty();
 	}
 
 }
