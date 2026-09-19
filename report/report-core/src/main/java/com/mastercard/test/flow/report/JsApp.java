e0b5020ea8c1c08f54a8133bb61c372b60166074
vchaitanya <41681992+vchaitanya@users.noreply.github.com>
2026-09-19 17:43:54 +0530

    Revert "Make report Writer thread-safe with write-once details and final-only…"

--- report/report-core/src/main/java/com/mastercard/test/flow/report/JsApp.java
+++ report/report-core/src/main/java/com/mastercard/test/flow/report/JsApp.java
@@ -143,22 +143,10 @@ class JsApp {
 	 */
 	public void write( Object payload, Path destination ) {
 		QuietFiles.createDirectories( destination.getParent() );
-		QuietFiles.write( destination, render( payload, destination ) );
-	}
-
-	/**
-	 * Renders an instance of this application without touching the disk
-	 *
-	 * @param payload     The data to insert into the index {@link Template}
-	 * @param destination Where the populated index file will be written to, which
-	 *                    determines the relative resource paths
-	 * @return The populated index file content
-	 */
-	public byte[] render( Object payload, Path destination ) {
-		return indexTemplate.insert(
+		QuietFiles.write( destination, indexTemplate.insert(
 				payload,
 				destination.getParent().relativize( originalIndexPath.getParent() ) )
-				.getBytes( UTF_8 );
+				.getBytes( UTF_8 ) );
 	}
 
 	private static void copy( InputStream in, FileOutputStream out ) throws IOException {
